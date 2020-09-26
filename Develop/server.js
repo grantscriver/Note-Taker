6fd72268b12f48fced5b09ced53d078fe956de87
Grant Scriver <grantscriver@gmail.com>
2020-09-26 17:29:09 -0500

    Switched the port and 3000 direction so that the deployment will work online

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,7 @@ const util = require("util");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.static("public"));
